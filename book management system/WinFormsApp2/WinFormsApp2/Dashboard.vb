6def218a8652d624f924042acbca85f991c2468c
afshan-codes <101944434+afshan-codes@users.noreply.github.com>
2023-03-12 15:34:51 +0530

    Add files via upload

---
+++ book management system/WinFormsApp2/WinFormsApp2/Dashboard.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Data.SqlClient
+Public Class Dashboard
+    Dim Con As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename= C: \Users\afshan barlaskar\Documents\Bookshopvbdb.mdf;Integrated Security=True;Connect Timeout=30")
+
+    Private Sub Dashboard_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim BookNum As Integer
+        Con.Open()
+        Dim sql = "select COUNT (*) from BookTbl "
+        Dim cmd As SqlCommand
+        cmd = New SqlCommand(sql, Con)
+        BookNum = cmd.ExecuteScalar
+        BookLbl.Text = BookNum
+        Con.Close()
+
+    End Sub
+End Class
---
+++ book management system/WinFormsApp2/WinFormsApp2/login.vb
@@ -0,0 +1,41 @@
+﻿Imports System.Data.SqlClient
+Public Class login
+    Dim Con As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename= C: \Users\afshan barlaskar\Documents\Bookshopvbdb.mdf;Integrated Security=True;Connect Timeout=30")
+    Dim cmd As SqlCommand
+    Private Sub LoginBtn_Click(sender As Object, e As EventArgs) Handles LoginBtn.Click
+        If UnameTb.Text = "" Or PasswordTb.Text = "" Then
+            MsgBox("enter username and password")
+        Else
+            Con.Open()
+            Dim query = "select * from UserTbl where name ='" & UnameTb.Text & "'and password ='" & PasswordTb.Text & "'"
+            cmd = New SqlCommand(query, Con)
+            Dim sda As SqlDataAdapter = New SqlDataAdapter(cmd)
+            Dim ds As DataSet = New DataSet()
+            sda.Fill(ds)
+            Dim a As Integer
+            a = ds.Tables(0).Rows.Count
+            If a = 0 Then
+                MsgBox("Wrong Username or Password")
+            Else
+                Dim Bill = New Bills
+                Bill.UserName = UnameTb.Text
+                Bill.Show()
+                Me.Hide()
+
+            End If
+
+            Con.Close()
+
+
+            End If
+    End Sub
+
+    Private Sub PictureBox4_Click(sender As Object, e As EventArgs) Handles PictureBox4.Click
+        Application.Exit()
+
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+End Class
